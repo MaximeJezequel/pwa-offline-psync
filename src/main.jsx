a667879a939ac900c1abc134e4f7bff69889407f
Maxime Jezequel <maxime.jezequel@gmail.com>
2023-05-05 22:19:47 +0200

    Add vite-plugin-pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { registerSW } from "virtual:pwa-register"
+import App from "./App.jsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log("offline ready")
+  },
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
