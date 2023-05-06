70623a739b8837e0089cc2979e0ad73603568ae0
Maxime Jezequel <maxime.jezequel@gmail.com>
2023-05-06 11:21:44 +0200

    Change icon color

--- vite.config.js
+++ vite.config.js
@@ -41,12 +41,6 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: "/maskable_icon.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
         ],
       },
     }),
