5729a6e0ca77cb6ea7f582f1ddfb511c0c99772d
Maxime Jezequel <maxime.jezequel@gmail.com>
2023-12-15 23:32:28 +0100

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import "./App.scss"
-import GamepadComponent from "./components/GamepadComponent"
+import { useState } from 'react';
+
+import Google from './components/Google/Google';
+import GamepadComponent from './components/GamePad/GamepadComponent';
+import BlackScreen from './components/BlackScreen/BlackScreen';
+
+import './App.scss';
 
 function App() {
+  const [page, setPage] = useState('Google');
+
+  const goToBlackScreenPage = () => {
+    console.log('go to black screen');
+    setPage('BlackScreen');
+  };
+
+  const goToGooglePage = () => {
+    console.log('go to google');
+    setPage('Google');
+  };
+
   return (
     <div className="App">
-      <GamepadComponent />
+      {/* <GamepadComponent /> */}
+      {page == 'Google' && <Google setPage={goToBlackScreenPage} />}
+      {page == 'BlackScreen' && <BlackScreen setPage={goToGooglePage} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { registerSW } from "virtual:pwa-register"
-import App from "./App.jsx"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
+import App from './App.jsx';
+import { DarkModeProvider } from './context/DarkModeContext';
+import './index.css';
 
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true)
+    if (confirm('New content available. Reload?')) {
+      updateSW(true);
     }
   },
   onOfflineReady() {
-    console.log("offline ready")
-  },
-})
+    console.log('offline ready');
+  }
+});
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
   </React.StrictMode>
-)
+);
