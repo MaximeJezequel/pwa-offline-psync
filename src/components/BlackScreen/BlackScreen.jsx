b4dade40e072aa649bd7f6c6d8fde444283e1a0b
Maxime Jezequel <maxime.jezequel@gmail.com>
2024-02-10 12:42:14 +0100

    Add fullscreen on black screen - test

--- src/components/BlackScreen/BlackScreen.jsx
+++ src/components/BlackScreen/BlackScreen.jsx
@@ -1,16 +1,53 @@
+import { useEffect } from 'react';
 import React from 'react';
 import GamepadComponent from '../GamePad/GamepadComponent';
 
 import './BlackScreen.scss';
 
 const BlackScreen = ({ setPage }) => {
+  useEffect(() => {
+    const requestFullScreen = () => {
+      const element = document.documentElement;
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.webkitRequestFullscreen) {
+        /* Safari */
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        /* IE11 */
+        element.msRequestFullscreen();
+      }
+    };
+
+    requestFullScreen(); // Automatically request fullscreen when component mounts
+
+    // Cleanup function to exit fullscreen when the component unmounts
+    return () => {
+      exitFullScreen();
+    };
+  }, []);
+
+  const exitFullScreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE11 */
+      document.msExitFullscreen();
+    }
+  };
+
   const handleGoogleClick = () => {
-    setPage()
-  }
+    setPage();
+  };
   return (
     <div className="night">
-      <div onClick={() => handleGoogleClick()}>toggle</div>
-      <GamepadComponent googleSearch = {handleGoogleClick}/>
+      <div className="redbutton" onClick={() => handleGoogleClick()}>
+        toggle
+      </div>
+      {/* <GamepadComponent googleSearch = {handleGoogleClick}/> */}
     </div>
   );
 };
